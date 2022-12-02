48e4dcd775f6af840abe410c87619ab66da74ee1
Riccardo Zoncada <riccardo.zoncada@xtreamers.io>
2022-12-02 16:16:46 +0100

    refactor: minor features computation refactor

--- src/preprocessing/fourier_features.py
+++ src/preprocessing/fourier_features.py
@@ -6,13 +6,16 @@ import numpy as np
 
 def create_fourier_features(dates: pd.DatetimeIndex, date_origin: datetime.date, weekly_harmonics: int, yearly_harmonics: int):
     """Create fourier terms as trigonometric functions of various frequencies"""
-
     features_df = pd.DataFrame(index=dates)
+
+    # Turn dates into integer incremental values to be used as the x-axis
     features_df['trend'] = (dates - pd.to_datetime(date_origin)).days
+
     for i in range(1, weekly_harmonics + 1):
         features_df[f'sin_7_{i}'] = np.sin(2 * np.pi * i / 7 * features_df['trend'])
         features_df[f'cos_7_{i}'] = np.cos(2 * np.pi * i / 7 * features_df['trend'])
     for i in range(1, yearly_harmonics + 1):
         features_df[f'sin_365_{i}'] = np.sin(2 * np.pi * i / 365.25 * features_df['trend'])
         features_df[f'cos_365_{i}'] = np.cos(2 * np.pi * i / 365.25 * features_df['trend'])
+
     return features_df
--- src/preprocessing/holiday_features.py
+++ src/preprocessing/holiday_features.py
@@ -4,8 +4,7 @@ import pandas as pd
 
 def create_holiday_features(dates: pd.DatetimeIndex):
     """Create holiday series, valued 0 or 1 depending on the day"""
-
-    features_df = pd.DataFrame(index=dates)
     holiday_cal = holidays.Italy()
+    features_df = pd.DataFrame(index=dates)
     features_df['holiday'] = dates.to_series().apply(lambda s: s in holiday_cal).astype(int)
     return features_df
