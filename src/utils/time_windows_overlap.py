5514bbfa618bea1bf70f2043321e6708d3b45da5
Riccardo Zoncada <riccardo.zoncada@xtreamers.io>
2022-12-02 16:18:18 +0100

    feat: add time windows overlap algorithm

---
+++ src/utils/time_windows_overlap.py
@@ -0,0 +1,12 @@
+import datetime
+from typing import Tuple, Optional
+
+
+def time_windows_overlap(date_rage_1: Tuple[datetime.date, datetime.date],
+                         date_rage_2: Tuple[datetime.date, datetime.date]) -> Optional[Tuple[datetime.date, datetime.date]]:
+    latest_start = max(date_rage_1[0], date_rage_2[0])
+    earliest_end = min(date_rage_1[1], date_rage_2[1])
+    delta = (earliest_end - latest_start).days + 1
+    overlap = max(0, delta)
+    if overlap:
+        return latest_start, earliest_end
