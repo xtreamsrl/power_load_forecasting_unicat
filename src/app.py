27fc405fc968dd6d375e68c3b36b877a2ada5bf5
Riccardo Zoncada <riccardo.zoncada@xtreamers.io>
2022-12-03 12:04:23 +0100

    feat: show line chart widescreen in dashboard

--- src/app.py
+++ src/app.py
@@ -10,9 +10,9 @@ from utils.time_windows_overlap import time_windows_overlap
 
 st.set_page_config(layout="wide")
 
-st.title('Power Load Forecasting Dashboard @ UniCatt')
-
 predictor = LinearRegressionPredictor()
+
+st.title('Power Load Forecasting Dashboard @ UniCatt')
 
 time_window = st.date_input(
     label='Select the forecast time window',
@@ -24,7 +24,7 @@ if len(time_window) == 2:
     predictor.predict(date_from=time_window[0], date_to=time_window[1])
 
     st.header("Prediction")
-    st.plotly_chart(predictor.get_predictions_fig())
+    st.plotly_chart(predictor.get_predictions_fig(), use_container_width=True)
 
     st.header("Model monitoring")
     uploaded_file = st.file_uploader(label="Actual CSV data:", type="csv")
@@ -45,16 +45,13 @@ if len(time_window) == 2:
                 "actual": actual_df[overlap[0]:overlap[1]].Load
             })
 
-            st.plotly_chart(px.line(actual_vs_predicted_df, title="Predictions vs Actual"))
-
-            mape = MAPE(actual_vs_predicted_df.actual, actual_vs_predicted_df.predicted)
-            st.metric(label="MAPE", value=f"{round(mape*100, 2)}%")
+            st.plotly_chart(px.line(actual_vs_predicted_df, title="Predicted vs Actual"), use_container_width=True)
 
             window_size = 3
             rolling_mape_df = pd.DataFrame(
                 {'rolling_mape': map(lambda window: MAPE(window.actual, window.predicted), actual_vs_predicted_df.rolling(window_size))},
                 index=actual_vs_predicted_df.index
             )
-            st.plotly_chart(px.line(rolling_mape_df[window_size:], title=f"{window_size} days rolling MAPE"))
+            st.plotly_chart(px.line(rolling_mape_df[window_size:], title=f"{window_size} days rolling MAPE"), use_container_width=True)
         else:
             st.error("No overlap between prediction and actual data time windows")
